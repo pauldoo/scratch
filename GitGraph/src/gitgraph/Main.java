707f9c95f41f5bc94d411eff789ab93de46b82ce
Paul Richards <paul.richards@gmail.com>
2010-03-26 22:12:51 +0000

    Now able to list all git objects.

--- GitGraph/src/gitgraph/Main.java
+++ GitGraph/src/gitgraph/Main.java
@@ -5,17 +5,48 @@
 
 package gitgraph;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author pauldoo
  */
 public class Main {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        Process process = null;
+        try {
+            String command[] = new String[]{"git",  "rev-list",  "--objects",  "--all"};
+            process = Runtime.getRuntime().exec(command);
+            process.getOutputStream().close();
+            process.getErrorStream().close();
+            BufferedReader r = new BufferedReader(new InputStreamReader(new BufferedInputStream(process.getInputStream())));
 
+            Pattern pattern = Pattern.compile("^([0123456789abcdef]{40}).*$");
+
+            String line;
+            while ((line = r.readLine()) != null) {
+                Matcher m = pattern.matcher(line);
+                if (m.matches()) {
+                    System.out.println("Object: " + m.group(1));
+                } else {
+                    System.err.println("Warning: I didn't understand '" + line + "'");
+                }
+            }
+            process.waitFor();
+            process = null;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (process != null) {
+                process.destroy();
+            }
+        }
+    }
 }
