f51e1eb5f3d26ea03b121e3247479e7dfc10f74f
Paul Richards <paul.richards@gmail.com>
2010-03-26 22:29:38 +0000

    Now adds all objects to a GraphViz 'dot' file (without any edges or object types being denoted).

--- GitGraph/src/gitgraph/Main.java
+++ GitGraph/src/gitgraph/Main.java
@@ -6,8 +6,13 @@
 package gitgraph;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,27 +25,45 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        final String outputFilename = args[0];
         Process process = null;
         try {
-            String command[] = new String[]{"git",  "rev-list",  "--objects",  "--all"};
-            process = Runtime.getRuntime().exec(command);
-            process.getOutputStream().close();
-            process.getErrorStream().close();
-            BufferedReader r = new BufferedReader(new InputStreamReader(new BufferedInputStream(process.getInputStream())));
-
-            Pattern pattern = Pattern.compile("^([0123456789abcdef]{40}).*$");
-
-            String line;
-            while ((line = r.readLine()) != null) {
-                Matcher m = pattern.matcher(line);
-                if (m.matches()) {
-                    System.out.println("Object: " + m.group(1));
-                } else {
-                    System.err.println("Warning: I didn't understand '" + line + "'");
+            {
+                String command[] = new String[]{"git",  "rev-list",  "--objects",  "--all"};
+                process = Runtime.getRuntime().exec(command);
+                process.getOutputStream().close();
+                process.getErrorStream().close();
+            }
+            Collection<String> objectHashes = new HashSet<String>();
+            {
+                BufferedReader r = new BufferedReader(new InputStreamReader(new BufferedInputStream(process.getInputStream())));
+                Pattern pattern = Pattern.compile("^([0123456789abcdef]{40}).*$");
+                {
+                    String line;
+                    while ((line = r.readLine()) != null) {
+                        Matcher m = pattern.matcher(line);
+                        if (m.matches()) {
+                            String objectHash = m.group(1);
+                            objectHashes.add(objectHash);
+                        } else {
+                            System.err.println("Warning: I didn't understand '" + line + "'");
+                        }
+                    }
                 }
             }
             process.waitFor();
             process = null;
+
+            {
+                PrintWriter dotOutput = new PrintWriter(new BufferedOutputStream(new FileOutputStream(outputFilename)));
+                dotOutput.println("strict digraph {");
+                for (String hash: objectHashes) {
+                    dotOutput.println("  \"" + hash + "\";");
+                }
+                dotOutput.println("}");
+                dotOutput.close();
+            }
+
         } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
