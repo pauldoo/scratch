8c74588550f747df9f5f12f953f0707c7525e208
Paul Richards <paul.richards@gmail.com>
2010-03-26 22:55:33 +0000

    Objects in GraphViz now have the correct shape.

--- GitGraph/src/gitgraph/Main.java
+++ GitGraph/src/gitgraph/Main.java
@@ -12,7 +12,7 @@ import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -28,14 +28,13 @@ public class Main {
         final String outputFilename = args[0];
         Process process = null;
         try {
+            Collection<String> objectHashes = new TreeSet<String>();
             {
                 String command[] = new String[]{"git",  "rev-list",  "--objects",  "--all"};
                 process = Runtime.getRuntime().exec(command);
                 process.getOutputStream().close();
                 process.getErrorStream().close();
-            }
-            Collection<String> objectHashes = new HashSet<String>();
-            {
+
                 BufferedReader r = new BufferedReader(new InputStreamReader(new BufferedInputStream(process.getInputStream())));
                 Pattern pattern = Pattern.compile("^([0123456789abcdef]{40}).*$");
                 {
@@ -43,23 +42,43 @@ public class Main {
                     while ((line = r.readLine()) != null) {
                         Matcher m = pattern.matcher(line);
                         if (m.matches()) {
-                            String objectHash = m.group(1);
-                            objectHashes.add(objectHash);
+                            String fullHash = m.group(1);
+                            objectHashes.add(fullHash);
                         } else {
                             System.err.println("Warning: I didn't understand '" + line + "'");
                         }
                     }
                 }
+                process.waitFor();
+                process = null;
             }
-            process.waitFor();
-            process = null;
 
             {
                 PrintWriter dotOutput = new PrintWriter(new BufferedOutputStream(new FileOutputStream(outputFilename)));
                 dotOutput.println("strict digraph {");
+
                 for (String hash: objectHashes) {
-                    dotOutput.println("  \"" + hash + "\";");
+                    String command[] = new String[]{"git", "cat-file", "-t", hash};
+                    process = Runtime.getRuntime().exec(command);
+                    process.getOutputStream().close();
+                    process.getErrorStream().close();
+
+                    BufferedReader r = new BufferedReader(new InputStreamReader(new BufferedInputStream(process.getInputStream())));
+                    String type = r.readLine();
+                    String shapeName = null;
+                    if ("commit".equals(type)) {
+                        shapeName = "circle";
+                    } else if ("tree".equals(type)) {
+                        shapeName = "triangle";
+                    } else if ("blob".equals(type)) {
+                        shapeName = "rectangle";
+                    }
+                    process.waitFor();
+                    process = null;
+
+                    dotOutput.println("  \"" + hash + "\" [label=\"" + hash.substring(0, 7) + "\",shape=" + shapeName + "];");
                 }
+
                 dotOutput.println("}");
                 dotOutput.close();
             }
