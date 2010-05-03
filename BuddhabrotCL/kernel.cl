1b544efa666a69f1e6cf2ab3ef08b4e1274b1f1a
Paul Richards <paul.richards@gmail.com>
2010-05-03 22:24:30 +0100

    Now creating much higher resolution results.

--- BuddhabrotCL/kernel.cl
+++ BuddhabrotCL/kernel.cl
@@ -1,5 +1,16 @@
 #pragma OPENCL EXTENSION cl_khr_global_int32_base_atomics
 
+__kernel void Memzero(
+    int numberOfElements,
+    __global int* output)
+{
+    const int min = (numberOfElements * (get_global_id(0) + 0)) / get_global_size(0);
+    const int max = (numberOfElements * (get_global_id(0) + 1)) / get_global_size(0);
+    for (int i = min; i < max; i++) {
+        output[i] = 0;
+    }
+}
+
 __kernel void Buddhabrot(
     float sampleMinX,
     float sampleMinY,
@@ -14,8 +25,8 @@ __kernel void Buddhabrot(
     int maximumIterations,
     __global int* outputImage)
 {
-    const float cR = sampleMinX + get_global_id(0) * (sampleMaxX - sampleMinX) / (get_global_size(0) - 1);
-    const float cI = sampleMinY + get_global_id(1) * (sampleMaxY - sampleMinY) / (get_global_size(1) - 1);
+    const float cR = sampleMinX + ((get_global_id(0) + 0.5) * (sampleMaxX - sampleMinX)) / get_global_size(0);
+    const float cI = sampleMinY + ((get_global_id(1) + 0.5) * (sampleMaxY - sampleMinY)) / get_global_size(1);
 
     bool escaped = false;
     {
@@ -42,19 +53,17 @@ __kernel void Buddhabrot(
             zR = tzR;
             zI = tzI;
 
-            if (zR >= imageMinX && zR < imageMaxX &&
-                zI >= imageMinY && zI < imageMaxY) {
-                int x = (zR - imageMinX) * (imageWidth - 1) / (imageMaxX - imageMinX);
-                int y = (zI - imageMinY) * (imageHeight - 1) / (imageMaxY - imageMinY);
-                if (x >= 0 && x < imageWidth &&
-                    y >= 0 && y < imageHeight) {
-                    __global int* address =
-                        outputImage +
-                        y * imageWidth +
-                        x;
+            int x = floor(((zR - imageMinX) * imageWidth) / (imageMaxX - imageMinX));
+            int y = floor(((zI - imageMinY) * imageHeight) / (imageMaxY - imageMinY));
+            if (x >= 0 && x < imageWidth &&
+                y >= 0 && y < imageHeight &&
+                !(x == 0 && y == 0)) {
+                __global int* address =
+                    outputImage +
+                    y * imageWidth +
+                    x;
 
-                    atom_inc(address);
-                }
+                atom_inc(address);
             }
         }
     }
--- BuddhabrotCL/main.cpp
+++ BuddhabrotCL/main.cpp
@@ -2,22 +2,25 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <sys/time.h>
 
 #define __CL_ENABLE_EXCEPTIONS
 
 #include "cl.hpp"
 
 namespace {
-    const int sampleWidth = 2000;
-    const int sampleHeight = 1500;
+    const int chunkWidth = 1000;
+    const int chunkHeight = 1000;
+    const int sampleWidth = 16000;
+    const int sampleHeight = 12000;
     const double sampleMinX = -2.5;
     const double sampleMinY = -1.5;
     const double sampleMaxX = 1.5;
     const double sampleMaxY = 1.5;
-    const int maximumIterations = 500;
+    const int maximumIterations = 20;
 
-    const int imageWidth = 640;
-    const int imageHeight = 480;
+    const int imageWidth = 3200;
+    const int imageHeight = 2400;
     const double imageMinX = -2.5;
     const double imageMinY = -1.5;
     const double imageMaxX = 1.5;
@@ -31,13 +34,20 @@ namespace {
         file.close();
         return buf.str();
     }
+
+    const double Time()
+    {
+        timeval tp;
+        gettimeofday(&tp, NULL);
+        return tp.tv_sec + tp.tv_usec * 1e-6;
+    }
 }
 
 int main(void) {
     try {
         const std::string kernelSource = ReadFileIntoString("kernel.cl");
 
-        cl::Context context(CL_DEVICE_TYPE_CPU);
+        cl::Context context(CL_DEVICE_TYPE_GPU);
         std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
         cl::Program::Sources sources;
         sources.push_back(std::make_pair(kernelSource.c_str(), kernelSource.size()));
@@ -54,48 +64,64 @@ int main(void) {
             throw;
         }
 
-        cl::Kernel kernel(program, "Buddhabrot");
-
         cl::CommandQueue queue(context, devices.front());
 
-        std::vector<int> resultBuffer(imageWidth * imageHeight);
-        const size_t outputBufferSize = resultBuffer.size() * sizeof(int);
+        const double startTime = Time();
+
+        const size_t outputBufferSize = imageWidth * imageHeight * sizeof(int);
         cl::Buffer outputBuffer(context, CL_MEM_READ_WRITE, outputBufferSize);
-        queue.enqueueWriteBuffer(
-            outputBuffer,
-            true,
-            0,
-            outputBufferSize,
-            &(resultBuffer.front()));
 
-        cl::KernelFunctor functor = kernel.bind(
-            queue,
-            cl::NDRange(sampleWidth, sampleHeight),
-            cl::NDRange());
+        const std::vector<cl::Event> memsetEvent(1,
+            cl::Kernel(program, "Memzero").bind(
+                    queue,
+                    cl::NDRange(1000),
+                    cl::NDRange()
+                )(
+                    imageWidth * imageHeight,
+                    outputBuffer
+                ));
 
-        cl::Event kernelEvent = functor(
-            static_cast<float>(sampleMinX),
-            static_cast<float>(sampleMinY),
-            static_cast<float>(sampleMaxX),
-            static_cast<float>(sampleMaxY),
-            imageWidth,
-            imageHeight,
-            static_cast<float>(imageMinX),
-            static_cast<float>(imageMinY),
-            static_cast<float>(imageMaxX),
-            static_cast<float>(imageMaxY),
-            maximumIterations,
-            outputBuffer);
+        std::vector<cl::Event> computeEvents;
+        for (int h = 0; h < sampleHeight; h += chunkHeight) {
+            for (int w = 0; w < sampleWidth; w += chunkWidth) {
+                const double chunkMinX = sampleMinX + ((sampleMaxX - sampleMinX) * (w + 0)) / sampleWidth;
+                const double chunkMinY = sampleMinY + ((sampleMaxY - sampleMinY) * (h + 0)) / sampleHeight;
+                const double chunkMaxX = sampleMinX + ((sampleMaxX - sampleMinX) * std::min((w + chunkWidth), sampleWidth)) / sampleWidth;
+                const double chunkMaxY = sampleMinY + ((sampleMaxY - sampleMinY) * std::min((h + chunkHeight), sampleHeight)) / sampleHeight;
+                computeEvents.push_back(cl::Kernel(program, "Buddhabrot").bind(
+                        queue,
+                        cl::NDRange(chunkWidth, chunkHeight),
+                        cl::NDRange(20, 20)
+                    )(
+                        static_cast<float>(chunkMinX),
+                        static_cast<float>(chunkMinY),
+                        static_cast<float>(chunkMaxX),
+                        static_cast<float>(chunkMaxY),
+                        imageWidth,
+                        imageHeight,
+                        static_cast<float>(imageMinX),
+                        static_cast<float>(imageMinY),
+                        static_cast<float>(imageMaxX),
+                        static_cast<float>(imageMaxY),
+                        maximumIterations,
+                        outputBuffer,
+                        &memsetEvent
+                    ));
+            }
+        }
 
-        std::vector<cl::Event> events;
-        events.push_back(kernelEvent);
+        std::vector<int> resultBuffer(imageWidth * imageHeight);
         queue.enqueueReadBuffer(
             outputBuffer,
             true,
             0,
             outputBufferSize,
             &(resultBuffer.front()),
-            &events);
+            static_cast<const std::vector<cl::Event>*>(&computeEvents));
+
+        const double endTime = Time();
+
+        std::wclog << L"OpenCL time: " << (endTime - startTime) << L"s\n";
 
         const int maxLevel = *std::max_element(resultBuffer.begin(), resultBuffer.end());
         std::wcout
