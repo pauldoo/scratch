38b059ecf1abee70e680afd26ce914a4860f66af
Paul Richards <paul.richards@gmail.com>
2010-07-21 20:32:02 +0100

    Refactoring 1000 switches solution.

--- Clojure/switches.clj
+++ Clojure/switches.clj
@@ -4,16 +4,13 @@
 ; http://programmerpuzzlers.com/which-switch/
 ; (it's cheating to code this, but it's an interesting exercise)
 
-(println
-    (count (filter
-       identity
-       (map
-           (fn
-               [n]
-               (odd? (count (filter
-                   identity
-                   (map
-                       (fn [val] (zero? (rem n val)))
-                       (range 1 1001))))))
-           (range 1 1001)))))
+(println (count
+    (filter
+        (fn
+            [n]
+            (odd? (count
+                (filter
+                    #(zero? (rem n %))
+                    (range 1 1001)))))
+        (range 1 1001))))
 
