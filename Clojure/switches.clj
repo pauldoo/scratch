cd930ce0b09a1a257a5e44b1e9e7785d02f7262c
Paul Richards <paul.richards@gmail.com>
2010-07-23 10:08:56 +0100

    Which switch is flipped most often, and how many times is it flipped?

--- Clojure/switches.clj
+++ Clojure/switches.clj
@@ -14,3 +14,18 @@
                     (range 1 1001)))))
         (range 1 1001))))
 
+(println
+    (let
+        [
+            counter-fn
+            (fn
+                [n]
+                (count
+                    (filter
+                        #(zero? (rem n %))
+                        (range 1 1001))))
+            switch-no
+            (apply max-key counter-fn (range 1 1001))
+        ]
+        [switch-no (counter-fn switch-no)]))
+
