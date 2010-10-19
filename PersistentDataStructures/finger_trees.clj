af421a1a7d01868e5741fbfb7665ae2fe9d6640a
Paul Richards <paul.richards@gmail.com>
2010-10-19 22:29:05 +0100

    Work in progress 2-3 Finger Trees in Clojure.

--- PersistentDataStructures/finger_trees.clj
+++ PersistentDataStructures/finger_trees.clj
@@ -1,7 +1,7 @@
 ; :mode=clojure:
 
-(defrecord node-2 [depth a b])
-(defrecord node-3 [depth a b c])
+(defrecord node-2 [a b])
+(defrecord node-3 [a b c])
 (defrecord finger-tree-single [v])
 (defrecord finger-tree-deep [l m r])
 
@@ -11,14 +11,18 @@
     (finger-tree-single. a))
 (defmethod push-front finger-tree-single [{r :v} a]
     (finger-tree-deep. [a] nil [r]))
-(defn make-node-3 [d a b c] (node-3. d a b c))
+(defn make-node-3 [a b c] (node-3. a b c))
 (defmethod push-front finger-tree-deep [{l :l m :m r :r} a]
     (if (<= (count l) 3)
         (finger-tree-deep. (cons a l) m r)
-        (finger-tree-deep. [a (first l)] (apply make-node-3 0 (rest l)) r))) ; not finished - needs to keep 'm'
+        (finger-tree-deep.
+            [a (first l)]
+            (push-front m (apply make-node-3 (rest l)))
+            r))) ; not finished - needs to keep 'm'
 
 (defn go [t n]
-    (if (<= n 0) nil (do (println t) (go (push-front t n) (dec n)))))
+    (do (println t)
+        (if (<= n 0) nil (go (push-front t n) (dec n)))))
 
 (go nil 20)
 
