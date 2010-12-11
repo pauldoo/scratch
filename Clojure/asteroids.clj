7cc39911e34be0d91ecba4e3d93132b46a842df6
Paul Richards <paul.richards@gmail.com>
2010-12-11 21:23:13 +0000

    Created a game update loop in a seperate thread.

--- Clojure/asteroids.clj
+++ Clojure/asteroids.clj
@@ -9,33 +9,54 @@
     '(java.awt.event.ActionListener)
 )
 
+(defn do-mod [coll func v]
+    (dosync (alter coll (fn [x] (apply func [x v])))))
+
+(defn create-component [game-state]
+    (doto
+        (proxy [javax.swing.JComponent] []
+            (paint [g] (dosync
+                (.setColor g (if (empty? (deref game-state)) (java.awt.Color/RED) (java.awt.Color/BLUE)))
+                (let [rect (.getClipBounds g)]
+                    (.fillRect g (.x rect) (.y rect) (.width rect) (.height rect))))))
+        (.setPreferredSize (new java.awt.Dimension 640 480))
+        (.setDoubleBuffered true)
+        (.setFocusable true)))
+
+(defn game-step [state keys-pressed]
+    (if (empty? keys-pressed)
+        (assoc (hash-map) :a true)
+        (hash-map)))
+
 (defn create-game []
-    (let [keys-pressed (ref #{})
-        result
-        (doto
-            (proxy [javax.swing.JComponent] []
-                (paint [g]
-                    (do
-                        (.setColor g (if (empty? (deref keys-pressed)) (java.awt.Color/RED) (java.awt.Color/BLUE)))
-                        (let [rect (.getClipBounds g)]
-                            (.fillRect g (.x rect) (.y rect) (.width rect) (.height rect))))))
-            (.setPreferredSize (new java.awt.Dimension 640 480))
-            (.setFocusable true)
-            (.addKeyListener (proxy [java.awt.event.KeyAdapter] []
-                (keyPressed [event] (if (not (.isConsumed event)) (do
-                    (dosync
-                        (alter keys-pressed (fn [x] (conj x (.getKeyCode event)))))
-                    (.consume event))))
-                (keyReleased [event] (if (not (.isConsumed event)) (do
-                    (dosync
-                        (alter keys-pressed (fn [x] (disj x (.getKeyCode event))))))))))
-        )]
+    (let [
+        keys-pressed (ref (hash-set))
+        game-state (ref (hash-map))
+        result (create-component game-state)
+        ]
         (do
+            (doto
+                result
+                (.addKeyListener (proxy [java.awt.event.KeyAdapter] []
+                    (keyPressed [event] (if (not (.isConsumed event)) (do
+                        (do-mod keys-pressed conj (.getKeyCode event))
+                        (.consume event))))
+                    (keyReleased [event] (if (not (.isConsumed event)) (do
+                        (do-mod keys-pressed disj (.getKeyCode event))
+                        (.consume event)))))))
+
             (doto
                 (new javax.swing.Timer (/ 1000 60)
                     (reify java.awt.event.ActionListener
                         (actionPerformed [this event] (.repaint result))))
                 (.start))
+
+            (future (loop []
+                (do
+                    (Thread/sleep 1)
+                    (dosync (alter game-state (fn [state] (game-step state (deref keys-pressed)))))
+                    (recur))))
+
             result)))
 
 (let [frame (new javax.swing.JFrame)]
