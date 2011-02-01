65b9922e36edff304026b7bffe05cb802852715f
Paul Richards <paul.richards@gmail.com>
2011-02-01 19:44:55 +0000

    Moving clojuroids into Leiningen project.

--- clojuroids/src/clojuroids/core.clj
+++
@@ -1,1 +0,0 @@
-(ns clojuroids.core)
--- clojuroids/test/clojuroids/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns clojuroids.test.core
-  (:use [clojuroids.core] :reload)
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
