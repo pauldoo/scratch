7461dce34b135551cf4a0f796b779d3890b71656
Paul Richards <paul.richards@gmail.com>
2011-02-20 16:48:02 +0000

    Introducing Marginalia to Clojuroids.

--- clojuroids/project.clj
+++ clojuroids/project.clj
@@ -1,6 +1,8 @@
+; :mode=clojure:
 (defproject clojuroids "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[marginalia "0.3.2"]]
   :main clojuroids)
 
