1fd458059d5dcda5e7d374e096ad8b32d541f4c7
Paul Richards <paul.richards@gmail.com>
2011-04-09 15:18:23 +0100

    Tinkering with the bracket puzzle.

---
+++ Clojure/brackets.clj
@@ -0,0 +1,24 @@
+; :mode=clojure:
+
+(defn bracket
+    ([n]
+        (bracket (* 2 n) 0))
+    ([r h]
+        (if (zero? r)
+            '("")
+            (concat
+                (if (> r h)
+                    (map
+                        (partial str "(")
+                        (bracket (dec r) (inc h))))
+                (if (> h 0)
+                    (map
+                        (partial str ")")
+                        (bracket (dec r) (dec h))))))))
+
+(apply println (bracket 0))
+(apply println (bracket 1))
+(apply println (bracket 2))
+(apply println (bracket 3))
+(apply println (bracket 4))
+
