45abaf854bbd2569b5df97415e0b57d56bb4b756
Paul Richards <paul.richards@gmail.com>
2011-05-22 13:53:36 +0100

    Fancy Fibonacci number generator.

---
+++ SICP/ex_1_19.clj
@@ -0,0 +1,27 @@
+; :mode=clojure:
+
+(defn T [p q]
+    (fn [a b] [
+        (+ (* b q) (* a q) (* a p))
+        (+ (* b p) (* a q))]))
+
+(defn id [a b] [a b])
+(defn comb-fn [A B]
+    (fn [a b] (apply A (B a b))))
+
+(defn K [n p q]
+    (if (= n 0)
+        (fn [a b] [a b])
+        (let
+            [r (K (quot n 2)
+                (+ (* p p) (* q q))
+                (+ (* 2 p q) (* q q)))]
+            (if (even? n)
+                r
+                (comb-fn (T p q) r)))))
+
+(defn fib [n]
+    (first ((K (dec n) 0 1) 1 0)))
+
+
+(fib 20)
