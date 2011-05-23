19398553d9d77506b3d0094bedfff8b9a4f6871b
Paul Richards <paul.richards@gmail.com>
2011-05-23 13:58:36 +0100

    Transformed to be tail recursive.

--- SICP/ex_1_19.clj
+++ SICP/ex_1_19.clj
@@ -2,27 +2,21 @@
 
 ; Fibonacci sequence in O(log(n)) iterations
 
-(defn T [p q]
-    (fn [a b] [
-        (+ (* b q) (* a q) (* a p))
-        (+ (* b p) (* a q))]))
+(defn T [p q a b] [
+    (+ (* b q) (* a q) (* a p))
+    (+ (* b p) (* a q))])
 
-(defn comb-fn [A B]
-    (fn [a b] (apply A (B a b))))
-
-(defn K [n p q]
-    (if (= n 0)
-        (fn [a b] [a b])
-        (let
-            [r (K (quot n 2)
+(defn K [n p q a b]
+    (if (= n 0) a
+        (let [[a b] (if (even? n) [a b] (T p q a b))]
+            (recur
+                (quot n 2)
                 (+ (* p p) (* q q))
-                (+ (* 2 p q) (* q q)))]
-            (if (even? n)
-                r
-                (comb-fn (T p q) r)))))
+                (+ (* 2 p q) (* q q))
+                a b))))
 
 (defn fib [n]
-    (first ((K (dec n) 0 1) 1 0)))
+    (K (dec n) 0 1 1 0))
 
 
 (fib 20)
