2b22ca3583dfdfb475822a4b7a0b534d3d152832
Paul Richards <paul.richards@gmail.com>
2011-05-29 13:15:53 +0100

    Added 'add' without repeated application of 'add-1'.

--- SICP/ex_2_6.clj
+++ SICP/ex_2_6.clj
@@ -1,5 +1,7 @@
 ; :mode=clojure:
 
+; Church numerals
+
 (def zero (fn [f] (fn [x] x)))
 
 (defn add-1 [n]
@@ -9,8 +11,13 @@
 
 (defn two [f] (fn [x] (f (f x))))
 
+(defn add [a b]
+    (fn [f] (fn [x] ((a f) ((b f) x)))))
+
 (println
     (((add-1 (add-1 (add-1 (add-1 zero)))) inc) 0)
     ((one inc) 0)
-    ((two inc) 0))
+    ((two inc) 0)
+    (((add one two) inc) 0)
+    (((add (add-1 (add-1 (add-1 zero))) (add-1 (add-1 zero))) inc) 0))
 
