9f43db6804fd459895226138fd646b2045a6d566
Paul Richards <paul.richards@gmail.com>
2011-05-29 13:08:24 +0100

    Added 'one' and 'two' without reference to 'add-1' or 'zero'.

--- SICP/ex_2_6.clj
+++ SICP/ex_2_6.clj
@@ -5,5 +5,12 @@
 (defn add-1 [n]
     (fn [f] (fn [x] (f ((n f) x)))))
 
+(defn one [f] (fn [x] (f x)))
 
+(defn two [f] (fn [x] (f (f x))))
+
+(println
+    (((add-1 (add-1 (add-1 (add-1 zero)))) inc) 0)
+    ((one inc) 0)
+    ((two inc) 0))
 
