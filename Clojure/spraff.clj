1356e99d8f5e6c82c3aa9e171df5a6776a3cdb08
Paul Richards <paul.richards@gmail.com>
2011-06-28 22:00:37 +0100

    Discovered 're-seq'.  :/

--- Clojure/spraff.clj
+++ Clojure/spraff.clj
@@ -10,14 +10,6 @@
     [s re]
     (vec (.split re s)))
 
-(defn extract
-    [s re]
-    (let [
-        m (.matcher re s)
-        f (fn [r] (if (.find m) (recur (conj r (.group m))) r))
-        ]
-    (f [])))
-
-(map #(extract % word-extractor-pattern) (split "This is a test. This is another." sentence-delimiter-pattern))
+(map (partial re-seq word-extractor-pattern) (split "This is a test. This is another." sentence-delimiter-pattern))
 
 
