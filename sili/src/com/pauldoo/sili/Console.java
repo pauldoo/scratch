cf36440a2697f844fcdb231a2254a12dd9150001
Paul Richards <paul.richards@gmail.com>
2011-07-19 21:19:19 +0100

    Skeleton NetBeans project for my crappy lisp interpreter.

---
+++ sili/src/com/pauldoo/sili/Console.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pauldoo.sili;
+
+/**
+ *
+ * @author pauldoo
+ */
+public class Console {
+    public static void main(String[] args) {
+    }
+}
