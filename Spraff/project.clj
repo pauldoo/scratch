77f19524081b63df71f43f3cf90a0b0b3333b3bc
Paul Richards <paul.richards@gmail.com>
2011-09-20 22:10:08 +0100

    Updating to Clojure 1.2.1.

--- Spraff/project.clj
+++ Spraff/project.clj
@@ -2,7 +2,7 @@
 (defproject spraff "1.0.0-SNAPSHOT"
     :description "IRC bot."
     :dependencies [
-        [org.clojure/clojure "1.2.0"]
+        [org.clojure/clojure "1.2.1"]
         [irclj "0.4.1-SNAPSHOT"]
         ]
     :dev-dependencies [
