1e7448bf871208f69aa46b0eda4f579a9fe306ab
Paul Richards <paul.richards@gmail.com>
2011-10-17 20:09:54 +0100

    Update to Clojure 1.3 - seems to work fine.

--- Spraff/project.clj
+++ Spraff/project.clj
@@ -2,10 +2,8 @@
 (defproject spraff "1.0.0-SNAPSHOT"
     :description "IRC bot."
     :dependencies [
-        [org.clojure/clojure "1.2.1"]
+        [org.clojure/clojure "1.3.0"]
         [irclj "0.4.1-SNAPSHOT"]
         ]
-    :dev-dependencies [
-        [marginalia "0.5.1"]]
     :main spraff.core)
 
