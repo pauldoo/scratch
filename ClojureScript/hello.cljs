c24fcf0373e37ff0a53ec273beee6f54423adf61
Paul Richards <paul.richards@gmail.com>
2011-11-11 22:49:15 +0000

    A little bit of HTML5 canvas.

--- ClojureScript/hello.cljs
+++ ClojureScript/hello.cljs
@@ -1,4 +1,22 @@
-(ns hello)
+; :mode=clojure:
+
+(ns hello
+    (:use [clojure.browser.dom :only [get-element]]))
+
 (defn ^:export greet [n]
-  (str "Hello " n))
+    (str "Hello " n))
+
+(def pos (atom 0))
+
+(defn ^:export foobar []
+    (let [canvas (get-element "my-canvas")
+        ctx (.getContext canvas "2d")
+        x (swap! pos inc)]
+        (do
+            (. ctx (save))
+            (.clearRect ctx 0 0 (.width canvas) (.height canvas))
+            (set! (.fillStyle ctx) "red")
+            (.fillRect ctx x 100 100 50)
+            (. ctx (restore))
+            )))
 
