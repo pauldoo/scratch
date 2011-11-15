c8893d29fcadf7ea64bf679991e3b8d91e460675
Paul Richards <paul.richards@gmail.com>
2011-11-15 21:00:18 +0000

    Work in progress.

--- ClojureScript/hello.cljs
+++ ClojureScript/hello.cljs
@@ -1,15 +1,15 @@
 ; :mode=clojure:
 
 (ns hello
-    (:use [clojure.browser.dom :only [get-element]]))
-
-(defn ^:export greet [n]
-    (str "Hello " n))
+    (:use [
+        goog.dom :only [getElement appendChild createDom]
+        goog.events :only [listen EventType]
+        ]))
 
 (def pos (atom 0))
 
 (defn ^:export foobar []
-    (let [canvas (get-element "my-canvas")
+    (let [canvas (goog.dom/getElement "my-canvas")
         ctx (.getContext canvas "2d")
         x (swap! pos inc)]
         (do
@@ -20,5 +20,21 @@
             (. ctx (restore))
             )))
 
-(defn ^:export test [] (foobar))
+(defn ^:export main []
+    (let [body (goog.dom/getElement "my-body")
+        f (partial goog.dom/appendChild body)] (do
+        (f
+            (goog.dom/createDom "h1" {} "Hello!"))
+        (f
+            (goog.dom/createDom "canvas" (.strobj {
+                "id" "my-canvas"
+                "width" 600
+                "height" 600
+                "style" "border: 1px solid #000000"
+                }) ""))
+        (f
+            (let [button (goog.dom/createDom "button" {} "Click!")] (do
+                ;(goog.events.listen button goog.events.EventType/CLICK foobar)
+                button)))
+)))
 
