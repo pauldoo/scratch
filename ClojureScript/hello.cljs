043aac251edb590785a58986bfa349a9928d116a
Paul Richards <paul.richards@gmail.com>
2011-11-17 23:02:44 +0000

    More experimenting with event listeners.

--- ClojureScript/hello.cljs
+++ ClojureScript/hello.cljs
@@ -9,14 +9,14 @@
 
 (def pos (atom 0))
 
-(defn foobar [canvas]
+(defn foobar [canvas y]
     (let [ctx (.getContext canvas "2d")
         x (swap! pos inc)]
         (do
             (. ctx (save))
             (.clearRect ctx 0 0 (.width canvas) (.height canvas))
             (set! (.fillStyle ctx) "red")
-            (.fillRect ctx x 100 100 50)
+            (.fillRect ctx x y 100 50)
             (. ctx (restore))
             )))
 
@@ -29,7 +29,9 @@
             }) "")
         button (goog.dom/createDom "button" {} "Click!")]
         (do
-            (goog.events/listen button goog.events.EventType/CLICK (partial foobar canvas))
+            (goog.events/listen button goog.events.EventType/CLICK (partial foobar canvas 100))
+            (goog.events/listen document.body goog.events.EventType/KEYDOWN (partial foobar canvas 50) true)
+            (goog.events/listen document.body goog.events.EventType/KEYUP (partial foobar canvas 150) true)
             (dorun (map (partial goog.dom/appendChild document.body)
                 [(goog.dom/createDom "h1" {} "Hello!") canvas button])))))
 
