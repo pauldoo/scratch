36e3741d13981a61052e42da5c7632ead136b674
Paul Richards <paul.richards@gmail.com>
2011-12-02 21:46:51 +0000

    Refactoring and using event timers for simulation and rendering cycles.

--- ClojureScript/hello.cljs
+++ ClojureScript/hello.cljs
@@ -6,40 +6,54 @@
         [goog.events :only [listen]]
         [goog.events.EventType :only [CLICK]]
         [goog.graphics :only [createGraphics Stroke SolidFill]]
+        [goog :only [Timer]]
         ))
 
-(def pos (atom 0))
+; State (AAAAHAHAHG, don't touch it!)
+(def pos (atom [0 100]))
 (def keys-pressed (atom (set)))
+
 (def my-code-url "https://github.com/pauldoo/scratch/blob/master/ClojureScript/hello.cljs")
 
 (defn print-to-log [v]
     (.log js/console v))
 
-(defn foobar [t canvas-element y event] (do
-    (print-to-log "hello")
+(defn input-event [event]
     (cond
-        (= t :up)
+        (= (.type event) goog.events.EventType/KEYUP)
             (swap! keys-pressed disj (.keyCode event))
-        (= t :down)
+        (= (.type event) goog.events.EventType/KEYDOWN)
             (swap! keys-pressed conj (.keyCode event))
         :else
-            nil)
-    (let [x (swap! pos inc)
+            nil))
+
+(defn tick []
+    (swap! pos (fn [[x y]] [(inc x) (* (count (deref keys-pressed)) 50)])))
+
+(defn rerender [canvas-element]
+    (let [[x y] (deref pos)
         graphics (goog.graphics/createGraphics 400 300)]
         (do
             (.drawCircle graphics x y 30 (Stroke. 2 "green") (SolidFill. "yellow"))
             (goog.dom/removeChildren canvas-element)
             (.render graphics canvas-element)
-            ))))
+            )))
 
 (defn ^:export main []
     (let [
         canvas (goog.dom/createDom "div" {} "")
-        button (goog.dom/createDom "button" {} "Click!")]
+        button (goog.dom/createDom "button" {} "Click!")
+        tick-timer (Timer. 100)
+        render-timer (Timer. 100)
+        ]
         (do
-            (goog.events/listen button goog.events.EventType/CLICK (partial foobar :click canvas 100))
-            (goog.events/listen document.body goog.events.EventType/KEYDOWN (partial foobar :down canvas 50) true)
-            (goog.events/listen document.body goog.events.EventType/KEYUP (partial foobar :up canvas 150) true)
+            (. tick-timer (start))
+            (. render-timer (start))
+            (goog.events/listen button goog.events.EventType/CLICK input-event)
+            (goog.events/listen document.body goog.events.EventType/KEYDOWN input-event true)
+            (goog.events/listen document.body goog.events.EventType/KEYUP input-event true)
+            (goog.events/listen tick-timer goog.Timer/TICK tick)
+            (goog.events/listen render-timer goog.Timer/TICK #(rerender canvas))
             (goog.dom/appendChild document.body (goog.dom/createDom "div" {}
                 (goog.dom/createDom "h1" {} "Work in progress..")
                 (goog.dom/createDom "div" {} "Try clicking the button, or pressing keys.")
