f4b2acdd48a871d78998bd506e44bd0c845fb9d1
Paul Richards <paul.richards@gmail.com>
2012-03-12 08:10:02 +0000

    Animation script for Juddhabrots 2-4.

--- BuddhabrotCL/animate.sh
+++ BuddhabrotCL/animate.sh
@@ -1,11 +1,26 @@
 #!/bin/zsh
 
 for i in `seq 10000 10999`; do
-    A=`perl -e "print sin(($i / 1000.0) * 2 * 3.1415) * 0.5 - 0.5;"`
-    B=`perl -e "print cos(($i / 1000.0) * 2 * 3.1415) * 0.5;"`
+    A=`perl -e "print sin(($i / 1000.0) * 2 * 3.1415) * 0.25 - 1.0;"`
+    B=`perl -e "print cos(($i / 1000.0) * 2 * 3.1415) * 0.25;"`
 
     echo $i $A $B
-    ./buddhabrot -2.5 -2.5 $A $B 2.5 2.5 $A $B > frameC_$i.pnm
+    ./buddhabrot -2.5 -2.5 $A $B 2.5 2.5 $A $B > frameDa_$i.pnm
 done
 
+for i in `seq 10000 10999`; do
+    A=`perl -e "print sin(($i / 1000.0) * 2 * 3.1415) * 0.26 - 1.0;"`
+    B=`perl -e "print cos(($i / 1000.0) * 2 * 3.1415) * 0.26;"`
+
+    echo $i $A $B
+    ./buddhabrot -2.5 -2.5 $A $B 2.5 2.5 $A $B > frameDb_$i.pnm
+done
+
+for i in `seq 10000 10999`; do
+    A=`perl -e "print sin(($i / 1000.0) * 2 * 3.1415) * 0.24 - 1.0;"`
+    B=`perl -e "print cos(($i / 1000.0) * 2 * 3.1415) * 0.24;"`
+
+    echo $i $A $B
+    ./buddhabrot -2.5 -2.5 $A $B 2.5 2.5 $A $B > frameDc_$i.pnm
+done
 
