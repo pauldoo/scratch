a5c4d368e9e082c8977a8e840fef24e2c3acf898
Paul Richards <paul.richards@gmail.com>
2012-06-11 21:43:39 +0100

    RWH examples.

---
+++ RealWorldHaskell/ch09/BetterPredicate.hs
@@ -0,0 +1,40 @@
+module BetterPredicate (betterFind) where
+
+import Control.Monad (filterM)
+import System.Directory (Permissions(..), getModificationTime, getPermissions)
+import System.Time (ClockTime(..))
+import System.FilePath (takeExtension)
+import Control.OldException (bracket, handle)
+import System.IO (IOMode(..), hClose, hFileSize, openFile)
+
+import RecursiveContents (getRecursiveContents)
+
+type Predicate = FilePath -> Permissions -> Maybe Integer -> ClockTime -> Bool
+
+getFileSize :: FilePath -> IO (Maybe Integer)
+
+betterFind :: Predicate -> FilePath -> IO [FilePath]
+
+{-
+-- Does not compile with lesser indentation ??
+betterFind p path = getRecursiveContents path >>= filterM check
+    where check name = do
+        perms <- getPermissions name
+        size <- getFileSize name
+        modified <- getModificationTime name
+        return (p name perms size modified)
+-}
+
+betterFind p path = getRecursiveContents path >>= filterM check
+    where check name = do
+            perms <- getPermissions name
+            size <- getFileSize name
+            modified <- getModificationTime name
+            return (p name perms size modified)
+
+getFileSize path = handle (\_ -> return Nothing) $
+    bracket (openFile path ReadMode) hClose $
+        \h -> do
+            size <- hFileSize h
+            return (Just size)
+
