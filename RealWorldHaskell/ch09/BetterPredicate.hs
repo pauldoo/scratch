0036a38ee51ee0312a12fd7c3975da992bd1b346
Paul Richards <paul.richards@gmail.com>
2012-06-13 23:27:32 +0100

    Real World Haskell examples.

--- RealWorldHaskell/ch09/BetterPredicate.hs
+++ RealWorldHaskell/ch09/BetterPredicate.hs
@@ -1,4 +1,4 @@
-module BetterPredicate (betterFind) where
+module BetterPredicate where
 
 import Control.Monad (filterM)
 import System.Directory (Permissions(..), getModificationTime, getPermissions)
@@ -15,16 +15,6 @@ getFileSize :: FilePath -> IO (Maybe Integer)
 
 betterFind :: Predicate -> FilePath -> IO [FilePath]
 
-{-
--- Does not compile with lesser indentation ??
-betterFind p path = getRecursiveContents path >>= filterM check
-    where check name = do
-        perms <- getPermissions name
-        size <- getFileSize name
-        modified <- getModificationTime name
-        return (p name perms size modified)
--}
-
 betterFind p path = getRecursiveContents path >>= filterM check
     where check name = do
             perms <- getPermissions name
@@ -37,4 +27,33 @@ getFileSize path = handle (\_ -> return Nothing) $
         \h -> do
             size <- hFileSize h
             return (Just size)
+
+
+myTest path _ (Just size) _ =
+    takeExtension path == ".cpp" && size > (128 * 1024)
+myTest _ _ _ _ = False
+
+
+type InfoP a = FilePath -> Permissions -> Maybe Integer -> ClockTime -> a
+
+pathP :: InfoP FilePath
+pathP path _ _ _ = path
+
+sizeP :: InfoP Integer
+sizeP _ _ (Just size) _ = size
+sizeP _ _ Nothing _ = -1
+
+equalP :: (Eq a) => InfoP a -> a -> InfoP Bool
+equalP f k w x y z = f w x y z == k
+
+-- betterFind (sizeP `equalP` 2312) "."
+
+liftP :: (a -> b -> c) -> InfoP a -> b -> InfoP c
+liftP q f k w x y z = f w x y z `q` k
+
+greaterP, lesserP :: (Ord a) => InfoP a -> a -> InfoP Bool
+greaterP = liftP (>)
+lesserP = liftP (<)
+
+-- betterFind (sizeP `lesserP` 2048)
 
