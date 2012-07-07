a32177d6c96e1f834138e59d42237b27f6b55592
Paul Richards <paul.richards@gmail.com>
2012-07-07 15:08:09 +0100

    Real World Haskell examples.

--- RealWorldHaskell/ch10/Parse.hs
+++ RealWorldHaskell/ch10/Parse.hs
@@ -2,9 +2,11 @@ module Parse where
 
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Lazy as L
+import Data.Char
 import Data.Int
 import Data.Word
-
+import Control.Applicative
+import PNM
 
 data ParseState = ParseState {
         string :: L.ByteString,
@@ -78,3 +80,92 @@ firstParser ==> secondParser =
                 Right (firstResult, newState) ->
                     runParse (secondParser firstResult) newState
 
+instance Functor Parse where
+    fmap f parser = parser ==> \result -> identity (f result)
+
+{--
+Prelude> :load Parse
+Ok, modules loaded: Parse.
+Prelude Parse> import qualified Data.ByteString.Lazy.Char8 as L8
+Prelude L8 Parse> import qualified Data.ByteString.Lazy as L
+Prelude L8 L Parse> parse parseByte L.empty
+Loading package bytestring-0.9.2.1 ... linking ... done.
+Left "byte offset 0: no more input"
+Prelude L8 L Parse> import Data.Functor
+Prelude L8 L Data.Functor Parse> parse (id <$> parseByte) L.empty
+Left "byte offset 0: no more input"
+Prelude L8 L Data.Functor Parse> let input = L8.pack "foo"
+Prelude L8 L Data.Functor Parse> L.head input
+102
+Prelude L8 L Data.Functor Parse> parse parseByte input
+Right 102
+Prelude L8 L Data.Functor Parse> parse (id <$> parseByte) input
+Right 102
+Prelude L8 L Data.Functor Parse> import Data.Char
+Prelude L8 L Data.Functor Data.Char Parse> parse ((chr . fromIntegral) <$> parseByte) input
+Right 'f'
+--}
+
+w2c :: Word8 -> Char
+w2c = chr . fromIntegral
+
+parseChar :: Parse Char
+parseChar = w2c <$> parseByte
+
+peekByte :: Parse (Maybe Word8)
+peekByte = (fmap fst . L.uncons . string) <$> getState
+
+peekChar :: Parse (Maybe Char)
+peekChar = fmap w2c <$> peekByte
+
+parseWhile :: (Word8 -> Bool) -> Parse [Word8]
+parseWhile p = (fmap p <$> peekByte) ==> \mp ->
+    if mp == Just True
+    then parseByte ==> \b ->
+        (b:) <$> parseWhile p
+    else identity []
+
+parseRawPGM =
+        parseWhileWith w2c notWhile ==> \header -> skipSpaces ==>&
+        assert (header == "P5") "invalid raw header" ==>&
+        parseNat ==> \width -> skipSpaces ==>&
+        parseNat ==> \height -> skipSpaces ==>&
+        parseNat ==> \maxGrey -> parseByte ==>&
+        parseBytes (width * height) ==> \bitmap ->
+            identity (Greymap width height maxGrey bitmap)
+    where
+        notWhile = (`notElem` " \r\n\t")
+
+parseWhileWith :: (Word8 -> a) -> (a -> Bool) -> Parse [a]
+parseWhileWith f p = fmap f <$> parseWhile (p . f)
+
+parseNat :: Parse Int
+parseNat = parseWhileWith w2c isDigit ==> \digits ->
+    if null digits
+    then bail "no more input"
+    else let n = read digits
+        in if n < 0
+            then bail "integer overflow"
+            else identity n
+
+(==>&) :: Parse a -> Parse b -> Parse b
+p ==>& f = p ==> \_ -> f
+
+skipSpaces :: Parse ()
+skipSpaces = parseWhileWith w2c isSpace ==>& identity ()
+
+assert :: Bool -> String -> Parse ()
+assert True _ = identity ()
+assert False err = bail err
+
+parseBytes :: Int -> Parse L.ByteString
+parseBytes n =
+    getState ==> \st ->
+    let n' = fromIntegral n
+        (h, t) = L.splitAt n' (string st)
+        st' = st {
+            offset = offset st + L.length h,
+            string = t }
+    in putState st' ==>&
+        assert (L.length h == n') "end of input" ==>&
+        identity h
