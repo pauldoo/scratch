4c30ac85fe304525744dee95ea51e7f727a2a57e
Paul Richards <paul.richards@gmail.com>
2012-11-03 23:04:50 +0000

    Tweak..

--- Tools/EncodeVob.sh
+++ Tools/EncodeVob.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-nice ffmpeg -i ${1} -filter:v yadif -s 768x576 -c:v libx264 -profile:v high -preset fast -crf 23 -c:a libfaac -ab 128k ${1}.mp4
+nice ffmpeg -i ${1} -filter:v yadif -s 768x576 -c:v libx264 -profile:v high -preset slow -crf 23 -c:a libfaac -ab 128k ${1}.mp4
 mp4creator -hint=1 ${1}.mp4
 mp4creator -hint=2 ${1}.mp4
 mp4creator -optimize ${1}.mp4
