bf6a7022b440db8db35a6a7cdf734309ca2030ce
Paul Richards <paul.richards@gmail.com>
2012-11-04 20:28:32 +0000

    Tweak cropping to maintain 4:3 display aspect ratio.

--- Tools/EncodeVob.sh
+++ Tools/EncodeVob.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 
-nice ffmpeg -i ${1} -filter:v yadif,scale=768:576,crop=736:560:16:8,deshake -c:v libx264 -profile:v high -preset medium -crf 23 -c:a libfaac -ab 128k ${1}.mp4
+# for i in *.vob; do ~/bin/nosleep ../EncodeVob.sh $i; done
+
+nice ffmpeg -i ${1} -filter:v yadif,scale=768:576,crop=736:552:16:12,deshake -c:v libx264 -profile:v high -preset medium -crf 23 -c:a libfaac -ab 128k ${1}.mp4
 mp4creator -hint=1 ${1}.mp4
 mp4creator -hint=2 ${1}.mp4
 mp4creator -optimize ${1}.mp4
