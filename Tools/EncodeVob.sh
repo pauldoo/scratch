6218551c28bba4e69d2c6d19b858a9c9503f5f00
Paul Richards <paul.richards@gmail.com>
2012-11-04 19:58:20 +0000

    Tweaks for cropping and deshaking (dad's recordings need this badly..)

--- Tools/EncodeVob.sh
+++ Tools/EncodeVob.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-nice ffmpeg -i ${1} -filter:v yadif -s 768x576 -c:v libx264 -profile:v high -preset medium -crf 23 -c:a libfaac -ab 128k ${1}.mp4
+nice ffmpeg -i ${1} -filter:v yadif,scale=768:576,crop=736:560:16:8,deshake -c:v libx264 -profile:v high -preset medium -crf 23 -c:a libfaac -ab 128k ${1}.mp4
 mp4creator -hint=1 ${1}.mp4
 mp4creator -hint=2 ${1}.mp4
 mp4creator -optimize ${1}.mp4
