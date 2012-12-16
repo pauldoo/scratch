bf2a013da27e7c61234f66d527598a5ff7ee238b
Paul Richards <paul.richards@gmail.com>
2012-12-16 12:09:16 +0000

    Moved to a slower preset for my final encode.

--- Tools/EncodeVob.sh
+++ Tools/EncodeVob.sh
@@ -2,7 +2,7 @@
 
 # for i in *.vob; do ~/bin/nosleep ../EncodeVob.sh $i; done
 
-nice ffmpeg -i ${1} -filter:v yadif,scale=768:576,crop=736:552:16:12,deshake -c:v libx264 -profile:v high -preset medium -crf 23 -c:a libfaac -ab 128k ${1}.mp4
+nice ffmpeg -i ${1} -filter:v yadif,scale=768:576,crop=736:552:16:12,deshake -c:v libx264 -profile:v high -preset slow -crf 23 -c:a libfaac -ab 128k ${1}.mp4
 mp4creator -hint=1 ${1}.mp4
 mp4creator -hint=2 ${1}.mp4
 mp4creator -optimize ${1}.mp4
