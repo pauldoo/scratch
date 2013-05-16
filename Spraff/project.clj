3896be0e5e51f5ace500036577aba3a24a6fdf51
Paul Richards <paul.richards@gmail.com>
2013-05-16 21:19:58 +0100

    Upgrading to Clojure v1.5.1, everything works pefectly, no changes to spraffbot code at all..  Amazing..

--- Spraff/project.clj
+++ Spraff/project.clj
@@ -2,7 +2,7 @@
 (defproject spraff "1.0.0-SNAPSHOT"
     :description "IRC bot."
     :dependencies [
-        [org.clojure/clojure "1.4.0"]
+        [org.clojure/clojure "1.5.1"]
         [irclj "0.4.1-SNAPSHOT"]
         [tuples "0.0.2"] ; v0.0.3 uses significantly more memory due to cached hascode values :/
         ]
