c0d8df8832c2e0e5fc8ea1776873700e7b7b3fa1
Paul Richards <paul.richards@gmail.com>
2013-06-09 20:49:32 +0100

    Experimenting with Jersey / json.

---
+++ notepad/src/main/java/notepad/FullPad.java
@@ -0,0 +1,15 @@
+package notepad;
+
+public class FullPad {
+
+    public final String username;
+    public final String padname;
+    public final String content;
+
+    public FullPad(final String username, final String padname, final String content) {
+        this.username = username;
+        this.padname = padname;
+        this.content = content;
+    }
+
+}
--- notepad/src/main/java/notepad/HelloWorld.java
+++ notepad/src/main/java/notepad/HelloWorld.java
@@ -2,18 +2,44 @@ package notepad;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
-@Path("/helloworld")
+import com.sun.jersey.api.json.JSONWithPadding;
+
+@Path("/pad/{username}/{padname}")
 public class HelloWorld {
 
-    // The Java method will process HTTP GET requests
     @GET
-    // The Java method will produce content identified by the MIME Media
-    // type "text/plain"
-    @Produces("text/plain")
-    public String getClichedMessage() {
-        // Return some cliched textual content
-        return "Hello World";
+    @Path("textonly")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getTextOnly(//
+            @PathParam("username") final String username, //
+            @PathParam("padname") final String padname) {
+        return "Cheese strings!";
     }
+
+    @GET
+    @Produces("application/x-javascript")
+    public JSONWithPadding getFullPad(//
+            @PathParam("username") final String username, //
+            @PathParam("padname") final String padname, //
+            @QueryParam("callback") final String callback) {
+        final FullPad result = new FullPad(username, padname, "Cheese strings!");
+        return new JSONWithPadding(result, callback);
+    }
+
+    @GET
+    @Path("metaonly")
+    @Produces("application/x-javascript")
+    public JSONWithPadding getMetadataOnly(//
+            @PathParam("username") final String username, //
+            @PathParam("padname") final String padname, //
+            @QueryParam("callback") final String callback) {
+        final FullPad result = new FullPad(username, padname, null);
+        return new JSONWithPadding(result, callback);
+    }
+
 }
