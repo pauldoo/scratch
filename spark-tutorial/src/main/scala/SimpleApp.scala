301165282174edfae5cdb2f84d8a494c5f85fdf1
Paul Richards <paul.richards@gmail.com>
2014-09-19 21:45:57 +0100

    Tweaking apache spark tutorial stuff.

--- spark-tutorial/src/main/scala/SimpleApp.scala
+++ spark-tutorial/src/main/scala/SimpleApp.scala
@@ -5,21 +5,28 @@ import org.apache.spark.SparkConf
 
 object SimpleApp {
   def main(args: Array[String]) {
+    val masterUrl = "local" // "spark://jam.lan:7077"
+    val conf = new SparkConf().setMaster(masterUrl).setAppName("Simple Application")
+
     val logFile = "/Users/pauldoo/Programs/spark-1.1.0/README.md" // Should be some file on your system
-    val conf = new SparkConf().setMaster("spark://jam.lan:7077").setAppName("Simple Application")
     val sc = new SparkContext(conf)
-    val logData = sc.textFile(logFile, 2).cache()
+    val logData = sc.textFile(logFile).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
     println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
 
+    var foobar = 0
     val wordCountsData = logData //
       .flatMap(_.split(" ")) //
-      .map(w => (w, 1)) //
+      .map(w => {
+        foobar += 1
+        (w, 1)
+      }) //
       .reduceByKey(_ + _)
     wordCountsData.cache()
     val wordCounts: List[(String, Int)] = wordCountsData.collect.toList
     println(s"Wordcounts: $wordCounts")
+    println(s"Foobar: $foobar")
 
     val averageLengthData: (Int, Int) = wordCountsData //
       .map(t => ((t._1.length() * t._2), t._2)) //
