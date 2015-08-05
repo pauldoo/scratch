a3d60e136753f2ea6e56e4c0d7b00456ee4f8de0
Paul Richards <paul.richards@gmail.com>
2015-08-05 19:58:08 +0000

    Silly script to dump JVM options.

---
+++ jvmopts/explore-defaults.sh
@@ -0,0 +1,10 @@
+#/bin/bash -e
+
+export COMMON_ARGS="-XX:+UnlockDiagnosticVMOptions -XX:+PrintFlagsFinal -version"
+java -server $COMMON_ARGS > base.txt
+java -server -XX:+UseConcMarkSweepGC $COMMON_ARGS > cms.txt
+java -server -XX:+UseG1GC $COMMON_ARGS > g1gc.txt
+java -server -XX:+AggressiveOpts $COMMON_ARGS > aggressiveopts.txt
+java -server -XX:+AggressiveHeap $COMMON_ARGS > aggressiveheap.txt
+
+
