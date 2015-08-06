6fe3a45b6c7477e44715b2b97d16c626543c08be
Paul Richards <paul.richards@gmail.com>
2015-08-06 10:04:13 +0100

    Adding parallel and parallelold options.

--- jvmopts/explore-defaults.sh
+++ jvmopts/explore-defaults.sh
@@ -6,5 +6,6 @@ java -server -XX:+UseConcMarkSweepGC $COMMON_ARGS > cms.txt
 java -server -XX:+UseG1GC $COMMON_ARGS > g1gc.txt
 java -server -XX:+AggressiveOpts $COMMON_ARGS > aggressiveopts.txt
 java -server -XX:+AggressiveHeap $COMMON_ARGS > aggressiveheap.txt
-
+java -server -XX:+UseParallelGC $COMMON_ARGS > parallel.txt
+java -server -XX:+UseParallelOldGC $COMMON_ARGS > parallelold.txt
 
