83bef05c6e0f311be4b2f4bdba3cb2ca54151b0f
Paul Richards <paul.richards@gmail.com>
2016-11-02 22:18:52 +0000

    Exercises 2.3, 2.4, 2.5

---
+++ fpis/src/main/scala/fpis/Exercise2p3p4p5.scala
@@ -0,0 +1,12 @@
+package fpis
+
+object Exercise2p3p4p5 {
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    a => b => f(a, b)
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
+}
