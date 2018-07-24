8603718993f8da1bbe04b57cf5bada31546f9b1c
Paul Richards <paul.richards@gmail.com>
2018-07-24 20:19:39 +0100

    Got logging working.

---
+++ timetrace2/README.md
@@ -0,0 +1,14 @@
+
+## Build and run
+
+### Debug
+
+```
+cargo build && RUST_LOG=info ./target/debug/timetrace2
+```
+
+### Release
+
+```
+cargo build --release && RUST_LOG=info ./target/release/timetrace2
+```
--- timetrace2/src/main.rs
+++ timetrace2/src/main.rs
@@ -1,8 +1,8 @@
-#[macro_use]
 extern crate memmap;
 extern crate rand;
 extern crate stopwatch;
-extern crate log;
+#[macro_use] extern crate log;
+extern crate env_logger;
 
 use memmap::MmapOptions;
 use memmap::MmapMut;
@@ -14,55 +14,57 @@ use rand::prelude::*;
 use stopwatch::Stopwatch;
 
 fn main() -> std::io::Result<()> {
+  env_logger::init();
+  info!("boop");
 
-  let fileName = "test.data";
+  let file_name = "test.data";
 
-{
-  let size :u64 = 4 * 1000 * 1000 * 1000;
-  let opsCount :u32 = 1000 * 1000;
+  {
+    let size :u64 = 2 * 1000 * 1000 * 1000;
+    let ops_count :u32 = 1000 * 1000;
+    info!("file will be {} bytes", size);
 
-  
-  let mut rng = thread_rng();
+    let mut rng = thread_rng();
 
-  let file_rw: File = OpenOptions::new()
-    .read(true)
-    .write(true)
-    .create_new(true)
-    .open(fileName)?;
-  file_rw.set_len(size)?;
+    let file_rw: File = OpenOptions::new()
+      .read(true)
+      .write(true)
+      .create_new(true)
+      .open(file_name)?;
+    file_rw.set_len(size)?;
 
-  let mut mmap_rw :MmapMut = unsafe { MmapOptions::new().map_mut(&file_rw)? };
+    let mut mmap_rw :MmapMut = unsafe { MmapOptions::new().map_mut(&file_rw)? };
 
-  let file_ro: File = OpenOptions::new()
-    .read(true)
-    .open(fileName)?;
-  
-  let mmap_ro : Mmap = unsafe { MmapOptions::new().map(&file_ro)? };
+    let file_ro: File = OpenOptions::new()
+      .read(true)
+      .open(file_name)?;
 
-  println!("value: {}", mmap_ro[3]);
+    let mmap_ro : Mmap = unsafe { MmapOptions::new().map(&file_ro)? };
 
-  let v :u8 = 32;
-  mmap_rw[3] = v;
+    info!("value: {}", mmap_ro[3]);
 
-  println!("value: {}", mmap_ro[3]);
+    let v :u8 = 32;
+    mmap_rw[3] = v;
+
+    info!("value: {}", mmap_ro[3]);
 
 
-  let mut sw = Stopwatch::start_new();
+    let mut sw = Stopwatch::start_new();
 
-  for _i in 1..=opsCount {
-      let idx :usize = rng.gen_range(0, size as usize);
-      mmap_rw[idx] += 1;
+    for _i in 1..=ops_count {
+        let idx :usize = rng.gen_range(0, size as usize);
+        mmap_rw[idx] += 1;
+    }
+    sw.stop();
+
+    // do something that takes some time
+    info!("{} ops took {}ms", ops_count, sw.elapsed_ms());
+    info!("closing files.");
   }
-  sw.stop();
+  info!("files closed, about to delete.");
 
-  // do something that takes some time
-  println!("{} ops took {}ms", opsCount, sw.elapsed_ms());
-  println!("closing files.");
-}
-println!("files closed, about to delete.");
-
-  fs::remove_file(fileName);
-println!("Deleted.");
+  fs::remove_file(file_name)?;
+  info!("Deleted.");
 
   Ok(())
   //let mmap = unsafe { MmapOptions::new().map(&file)? };
