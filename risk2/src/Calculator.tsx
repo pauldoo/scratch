7739e251c96ccb1b216acb7199ec8964d919d74c
Paul Richards <paul.richards@gmail.com>
2021-02-01 21:02:49 +0000

    WIP.

--- risk2/src/Calculator.tsx
+++ risk2/src/Calculator.tsx
@@ -1,5 +1,6 @@
-import { useMemo, useState } from 'react';
+import { useMemo, useState, Fragment } from 'react';
 import { Bar } from 'react-chartjs-2';
+import { getAutomaticTypeDirectiveNames } from 'typescript';
 import { OutcomeDistribution, simulate, State } from './Simulation';
 
 export interface Props {
@@ -10,7 +11,7 @@ const Outcomes: React.FC<State> = (props) => {
     
     const simData: OutcomeDistribution = useMemo( () => {
         return simulate(props); 
-    }, [props.attackerCount, props.defenderCount]);
+    }, [props]);
 
     const labels :string[] = simData.outcomes.map( (e) => {
         return e.state.attackerCount + " - " + e.state.defenderCount;
@@ -24,30 +25,82 @@ const Outcomes: React.FC<State> = (props) => {
         labels: labels,
         datasets: [
           {
-            label: 'Liklihood game ends with <attacker> vs <defender>',
-            backgroundColor: 'rgba(255,99,132,0.2)',
-            borderColor: 'rgba(255,99,132,1)',
-            borderWidth: 1,
-            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-            hoverBorderColor: 'rgba(255,99,132,1)',
-            data: values
+            label: 'Liklihood',
+            backgroundColor: 'rgba(255,99,132,0.5)',
+            //borderColor: 'rgba(255,99,132,1)',
+            //borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255,99,132,1.0)',
+            //hoverBorderColor: 'rgba(255,99,132,1)',
+            data: values,
+            barPercentage: 1.0,
+            categoryPercentage: 1.0
           }
         ]
       };
 
+    const options = {
+        title: {
+            display: true,
+            text: 'End state likelihoods'
+        },
+        scales: {
+            xAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Remaining troops ([attacker] - [defender])'
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Likelihood (%)'
+                }
+            }]            
+        },
+        legend: {
+            display: false
+        }
+    };
+
     return (
-        <div>
-            <h2></h2>
-            <p>All out attacks with {props.attackerCount} against {props.defenderCount}</p>
-            <Bar data={data} />
-            <p>Liklihood attacker wins: {(100 * simData.attackerWinCount) / simData.totalCount}%</p>
-            <p>Liklihood defender wins: {(100 * simData.defenderWinCount) / simData.totalCount}%</p>
-            <p>p25: {simData.p25.attackerCount} - {simData.p25.defenderCount}</p>
-            <p>p50: {simData.p50.attackerCount} - {simData.p50.defenderCount}</p>
-            <p>p75: {simData.p75.attackerCount} - {simData.p75.defenderCount}</p>
-            <p>(Simulated {simData.totalCount} tials in {simData.simulationTimeMs}ms.)</p>
-        </div>
-    )
+        <Fragment>
+            <div>
+                <h2>All out attack with {props.attackerCount} against {props.defenderCount}</h2>
+                <Bar data={data} options={options} />
+            </div>
+            <div>
+                <h2>Summary</h2>
+                <div className='tablewrapper'>
+                    <table>
+                        <tr>
+                            <td>Liklihood attacker wins</td>
+                            <td>{(100 * simData.attackerWinCount) / simData.totalCount}%</td>
+                        </tr>
+                        <tr>
+                            <td>Liklihood defender wins</td>
+                            <td>{(100 * simData.defenderWinCount) / simData.totalCount}%</td>
+                        </tr>
+                        <tr>
+                            <td>P25</td>
+                            <td>{simData.p25.attackerCount} - {simData.p25.defenderCount}</td>
+                        </tr>
+                        <tr>
+                            <td>P50</td>
+                            <td>{simData.p50.attackerCount} - {simData.p50.defenderCount}</td>
+                        </tr>
+                        <tr>
+                            <td>P75</td>
+                            <td>{simData.p75.attackerCount} - {simData.p75.defenderCount}</td>
+                        </tr>
+                    </table>
+                </div>
+                <p><small>(Simulated {simData.totalCount} tials in {simData.simulationTimeMs}ms.)</small></p>
+            </div>
+        </Fragment>
+    );
 }
 
 export const Calculator: React.FC<Props> = (props) => {
@@ -63,28 +116,39 @@ export const Calculator: React.FC<Props> = (props) => {
     }
 
     return (
-        <div>
+        <Fragment>
             <div>
-                Attackers:
-                <input
-                    type="number"
-                    min="2"
-                    max="100"
-                    value={attackerCount}
-                    onChange={(e) => setAttackerCount(Number(e.target.value))}
-                />
-
-                Defenders:
-                <input
-                    type="number"
-                    min="1"
-                    max="100"
-                    value={defenderCount}
-                    onChange={(e) => setDefenderCount(Number(e.target.value))}
-                />
-            
+                <h2>Setup</h2>
+                <div className='tablewrapper'>
+                    <table>
+                        <tr>
+                            <td>Attackers:</td>
+                            <td>
+                                <input
+                                    type="number"
+                                    min="2"
+                                    max="100"
+                                    value={attackerCount}
+                                    onChange={(e) => setAttackerCount(Number(e.target.value))}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Defenders:</td>
+                            <td>
+                                <input
+                                    type="number"
+                                    min="1"
+                                    max="100"
+                                    value={defenderCount}
+                                    onChange={(e) => setDefenderCount(Number(e.target.value))}
+                                />
+                            </td>
+                        </tr>
+                    </table>
+                </div>
             </div>
             {calculator}
-        </div>
-    )
+        </Fragment>
+    );
 }
